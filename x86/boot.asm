6fe6121a1b63f9898eec5459e96e9a65d6dd790c
Steven Kang <kangweibaby@163.com>
2021-02-04 15:06:08 +0800

    chapter 5

--- x86/boot.asm
+++ x86/boot.asm
@@ -1,31 +1,105 @@
-section mbr vstart=0x7c00
-    mov ax, cs
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    mov fs, ax
-    mov sp, 0x7c00
+    base equ 0x7c00;
+    video equ 0xb800;
 
-    mov ax, 0x600
-    mov bx, 0x700
-    mov cx, 0
-    mov dx, 0x184f
-    int 0x10
+    mov ax, 0x0003; clear screen
+    int 0x10;
 
-    mov ah, 3
-    mov bh, 0
-    int 0x10
+    mov ax,0xb800                 ;指向文本模式的显示缓冲区
+    mov es,ax
 
-    mov ax, message
-    mov bp, ax
+    ;以下显示字符串"Label offset:"
+    mov byte [es:0x00],'L'
+    mov byte [es:0x01],0x07
+    mov byte [es:0x02],'a'
+    mov byte [es:0x03],0x07
+    mov byte [es:0x04],'b'
+    mov byte [es:0x05],0x07
+    mov byte [es:0x06],'e'
+    mov byte [es:0x07],0x07
+    mov byte [es:0x08],'l'
+    mov byte [es:0x09],0x07
+    mov byte [es:0x0a],' '
+    mov byte [es:0x0b],0x07
+    mov byte [es:0x0c],"o"
+    mov byte [es:0x0d],0x07
+    mov byte [es:0x0e],'f'
+    mov byte [es:0x0f],0x07
+    mov byte [es:0x10],'f'
+    mov byte [es:0x11],0x07
+    mov byte [es:0x12],'s'
+    mov byte [es:0x13],0x07
+    mov byte [es:0x14],'e'
+    mov byte [es:0x15],0x07
+    mov byte [es:0x16],'t'
+    mov byte [es:0x17],0x07
+    mov byte [es:0x18],':'
+    mov byte [es:0x19],0x07
 
-    mov cx, 5
-    mov ax, 0x1301
-    mov bx, 0x2
-    int 0x10
+    mov ax,number                 ;取得标号number的偏移地址
+    mov bx,10
 
-    jmp $
 
-    message db "1 mbr\0"
-    times 510 - ($-$$) db 0
-    db 0x55, 0xaa
+    ;设置数据段的基地址
+    mov cx,cs
+    mov ds,cx
+
+    ;求个位上的数字
+    mov dx,0
+    div bx
+    mov [0x7c00+number+0x00],dl   ;保存个位上的数字
+
+    ;求十位上的数字
+    xor dx,dx
+    div bx
+    mov [0x7c00+number+0x01],dl   ;保存十位上的数字
+
+    ;求百位上的数字
+    xor dx,dx
+    div bx
+    mov [0x7c00+number+0x02],dl   ;保存百位上的数字
+
+    ;求千位上的数字
+    xor dx,dx
+    div bx
+    mov [0x7c00+number+0x03],dl   ;保存千位上的数字
+
+    ;求万位上的数字 
+    xor dx,dx
+    div bx
+    mov [0x7c00+number+0x04],dl   ;保存万位上的数字
+
+    ;以下用十进制显示标号的偏移地址
+    mov al,[0x7c00+number+0x04]
+    add al,0x30
+    mov [es:0x1a],al
+    mov byte [es:0x1b],0x04
+    
+    mov al,[0x7c00+number+0x03]
+    add al,0x30
+    mov [es:0x1c],al
+    mov byte [es:0x1d],0x04
+    
+    mov al,[0x7c00+number+0x02]
+    add al,0x30
+    mov [es:0x1e],al
+    mov byte [es:0x1f],0x04
+
+    mov al,[0x7c00+number+0x01]
+    add al,0x30
+    mov [es:0x20],al
+    mov byte [es:0x21],0x04
+
+    mov al,[0x7c00+number+0x00]
+    add al,0x30
+    mov [es:0x22],al
+    mov byte [es:0x23],0x04
+    
+    mov byte [es:0x24],'D'
+    mov byte [es:0x25],0x07
+    
+infi: jmp near infi                 ;无限循环
+
+number db 0,0,0,0,0
+
+times   510 - ($ - $$) db 0
+        dw 0xaa55; db 0x55,0xaa
