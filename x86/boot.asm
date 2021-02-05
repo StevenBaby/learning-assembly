b7a5e300015192c6139135c3611f62113ca7e097
Steven Kang <kangweibaby@163.com>
2021-02-05 15:41:40 +0800

    update chapter 7

--- x86/boot.asm
+++ x86/boot.asm
@@ -16,12 +16,9 @@ print_loop:
 print_done:
     ret
 
-test:
-
-    xchg bx, bx
-    ret
-
 _start:
+    ; xchg bx, bx;
+
     mov ax, cs;
     mov ds, ax;
     mov ss, ax;
@@ -33,36 +30,42 @@ _start:
     mov ax,video  ;指向文本模式的显示缓冲区
     mov es,ax
 
-    ;以下显示字符串"Label offset:"
+    ;以下显示字符串 message
     mov si, message
     call print
 
-    mov ax,number  ;取得标号number的偏移地址
-    mov cx, 5;
-number_loop:
+    xor ax, ax
+    mov cx, 100
 
-    mov bx,10
-    mov dx,0
+calculate:
+    add ax, cx
+    loop calculate
+
+    ; xchg bx, bx;
+
+    xor cx, cx
+split:
+    mov bx, 10;
+    mov dx, 0;
     div bx ; (dx ax) / bx = 商(ax) 余数(dx)
 
-    mov bx, dx;
-    mov dl, [bx + string]
+    add dl, 0x30;
+    push dx
 
-    mov bx, cx;
-    mov [bx + number - 1], dl
+    ; xchg bx, bx;
 
-    loop number_loop
+    inc cx
+    cmp ax, 0
+    jne split
 
-    mov si, number
-    call print
-
-    call test
+show_result:
+    pop ax
+    mov ah, 0x0e ; 0000 黑色背景 1110 浅灰色，默认颜色
+    int 0x10;
+    loop show_result
 
 infi: jmp near infi                 ;无限循环
 
-string: db '0123456789ABCDEF', 0
-
-number db 0,0,0,0,0, 'D', 0
-message db "Label offset:", 0; 字符串以 0 结尾
+message db "1+2+3+...+99+100=", 0; 字符串以 0 结尾
 times   510 - ($ - $$) db 0
         dw 0xaa55; db 0x55,0xaa
