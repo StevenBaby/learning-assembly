6a0e5a16d86aa44e35774f0317b3604529f46815
Steven Kang <kangweibaby@163.com>
2021-02-27 22:35:35 +0800

    add chapter 13

--- x86/app.asm
+++ x86/app.asm
@@ -1,77 +1,87 @@
+         ;代码清单13-3
+         ;文件名：c13.asm
+         ;文件说明：用户程序 
+         ;创建日期：2011-10-30 15:19   
+         
+;===============================================================================
+SECTION header vstart=0
 
-section header vstart=0;
-    length          dd program_end
-    entry           dw start; 偏移地址
-                    dd section.code.start; 段地址
-    table           dw (header_end - code_segment)/4 ; 段重定位表项个数
-    code_segment    dd section.code.start;
-    data_segment    dd section.data.start;
-    stack_segment   dd section.stack.start;
+         program_length   dd program_end          ;程序总长度#0x00
+         
+         head_len         dd header_end           ;程序头部的长度#0x04
+
+         stack_seg        dd 0                    ;用于接收堆栈段选择子#0x08
+         stack_len        dd 1                    ;程序建议的堆栈大小#0x0c
+                                                  ;以4KB为单位
+                                                  
+         prgentry         dd start                ;程序入口#0x10 
+         code_seg         dd section.code.start   ;代码段位置#0x14
+         code_len         dd code_end             ;代码段长度#0x18
+
+         data_seg         dd section.data.start   ;数据段位置#0x1c
+         data_len         dd data_end             ;数据段长度#0x20
+             
+;-------------------------------------------------------------------------------
+         ;符号地址检索表
+         salt_items       dd (header_end-salt)/256 ;#0x24
+         
+         salt:                                     ;#0x28
+         PrintString      db  '@PrintString'
+                     times 256-($-PrintString) db 0
+                     
+         TerminateProgram db  '@TerminateProgram'
+                     times 256-($-TerminateProgram) db 0
+                     
+         ReadDiskData     db  '@ReadDiskData'
+                     times 256-($-ReadDiskData) db 0
+                 
 header_end:
 
-section code align=16 vstart=0
+;===============================================================================
+SECTION data vstart=0    
+                         
+         buffer times 1024 db  0         ;缓冲区
 
-start:
-    ; xchg bx, bx
+         message_1         db  0x0d,0x0a,0x0d,0x0a
+                           db  '**********User program is runing**********'
+                           db  0x0d,0x0a,0
+         message_2         db  '  Disk data:',0x0d,0x0a,0
 
-    mov ax, [stack_segment]
-    mov ss, ax
-    mov sp, stack_end
-
-    mov ax, [data_segment]
-    mov ds, ax
-
-    call clear_screen
-
-    mov si, message
-    call print
-
-    .idle:
-        call get_key
-        ; hlt                                ;使CPU进入低功耗状态，直到用中断唤醒
-        jmp .idle
-
-    jmp $
-
-clear_screen:
-    mov ax, 0x3
-    int 0x10;
-    ret
-
-get_key:
-    mov ah, 0x00
-    int 0x16
-
-    mov ah, 0x0e
-    int 0x10;
-
-    ret
-
-print:
-    cld
-    .print_loop:
-        lodsb
-        or al, al
-        jz .print_done
-
-        mov ah, 0x0e ; 0000 黑色背景 1110 浅灰色，默认颜色
-        int 0x10;
-        jmp .print_loop
-
-    .print_done:
-        ret
-
-code_end:
-
-section data align=16 vstart=0;
-    message db 'Hello world!!!', 0
 data_end:
 
-section stack align=16 vstart=0;
-    ; resb 256
-    times 0x100 db 0; 干掉 warning
-stack_end:
+;===============================================================================
+      [bits 32]
+;===============================================================================
+SECTION code vstart=0
+start:
+         mov eax,ds
+         mov fs,eax
+     
+         mov eax,[stack_seg]
+         mov ss,eax
+         mov esp,0
+     
+         mov eax,[data_seg]
+         mov ds,eax
+     
+         mov ebx,message_1
+         call far [fs:PrintString]
+     
+         mov eax,100                         ;逻辑扇区号100
+         mov ebx,buffer                      ;缓冲区偏移地址
+         call far [fs:ReadDiskData]          ;段间调用
+     
+         mov ebx,message_2
+         call far [fs:PrintString]
+     
+         mov ebx,buffer 
+         call far [fs:PrintString]           ;too.
+     
+         jmp far [fs:TerminateProgram]       ;将控制权返回到系统 
+      
+code_end:
 
-section trail align=16;
-    ending db 'program ending', 0
+;===============================================================================
+SECTION trail
+;-------------------------------------------------------------------------------
 program_end:
