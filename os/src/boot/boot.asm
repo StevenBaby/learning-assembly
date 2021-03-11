8e449bc32c72c0183bdc079bd3198e7115b0ef63
Steven Kang <kangweibaby@163.com>
2021-03-11 16:36:16 +0800

    add loader

--- os/src/boot/boot.asm
+++ os/src/boot/boot.asm
@@ -1,7 +1,6 @@
-boot_base equ 0x7c00;
-seek_loader equ 1;
+%include "boot.inc"
 
-section boot align=16 vstart=boot_base
+section boot align=16 vstart=BOOT_BASE_ADDR
 
     ;clean screen
     mov ax, 3
@@ -13,16 +12,15 @@ section boot align=16 vstart=boot_base
     mov es, ax
     mov ss, ax
     mov fs, ax
-    mov sp, boot_base
+    mov sp, BOOT_BASE_ADDR
 
     ;print message
     mov si, message
     call print
+
     call load_loader
 
-    xchg bx, bx
-    jmp 0x1000:0
-
+    jmp 0:LOADER_BASE_ADDR
 
 finish:
     sti ; open interrupt
@@ -31,65 +29,73 @@ finish:
 
 load_loader:
     ; read first sector
-    mov ax, [cs:load_base]
-    mov dx, [cs:load_base + 2]
-    mov bx, 16
-    div bx
-    mov ds, ax
-    mov es, ax
-
-    xor di, di
-    mov si, seek_loader
-    xor bx, bx
-
-    call read_harddisk_sector
+    mov eax, LOADER_START_SECTOR
+    mov bx, LOADER_BASE_ADDR
+    mov cx, 20 ; 10KB
+    call read_disk
 
     ret
 
+read_disk: 
+    ;从硬盘读取一个逻辑扇区
+    ; EAX = 逻辑扇区号
+    ; DS: EBX = 目标缓冲区地址
+    ; CX, 读取的扇区数量
 
-read_harddisk_sector:   ;从硬盘读取一个逻辑扇区
-                        ; EAX=逻辑扇区号
-                        ; DS:EBX=目标缓冲区地址
-                        ;返回：EBX=EBX+512
         pusha
 
-        mov dx,0x1f2
-        mov al,1
-        out dx,al ;读取的扇区数
+        push eax
 
-        inc dx                              ;0x1f3
-        mov ax, si
-        out dx, al                       ;LBA地址7~0
+        ; 设置需要读取的扇区数
+        mov dx, 0x1f2
+        mov al, cl
+        out dx, al
 
-        inc dx                          ;0x1f4
-        mov al, ah
-        out dx, al                       ;LBA地址15~8
+        pop eax
 
-        inc dx                          ;0x1f5
-        mov ax, di
-        out dx, al                       ;LBA地址23~16
+        ; 写入 LBA地址 7 ~ 0
+        inc dx  ;0x1f3
+        out dx, al;
 
-        inc dx                          ;0x1f6
-        mov al, 0xe0
-        or al, ah                      ;第一硬盘  LBA地址27~24
+        ; 写入 LBA地址 15 ~ 8
+        inc dx ;0x1f4
+        shr eax, 8
+        out dx, al
+
+        ; 写入 LBA地址 23 ~ 16
+        inc dx ;0x1f5
+        shr eax, 8
+        out dx, al
+
+        ; 写入 第一硬盘  LBA地址27~24
+        inc dx ;0x1f6
+        shr eax, 8
+        and al, 0x0f
+        or al, 0xe0
         out dx,al
 
-        inc dx                          ;0x1f7
-        mov al,0x20                     ;读命令
+        ; 写入读命令
+        inc dx ;0x1f7
+        mov al, 0x20
         out dx,al
 
     .waits:
         in al,dx
         and al,0x88
         cmp al,0x08
-        jnz .waits                      ;不忙，且硬盘已准备好数据传输 
+        jnz .waits ; 不忙，且硬盘已准备好数据传输 
 
-        mov cx,256                     ;总共要读取的字数
-        mov dx,0x1f0
+        mov ax, cx
+        mov dx, 256 ;总共要读取的字数
+        mul dx
+        mov cx, ax
+        mov dx, 0x1f0 ; 读取端口号
+
     .readw:
-        in ax,dx
-        mov [bx],ax
-        add bx,2
+        in ax,dx ; 读取输入
+        mov [bx], ax ; 转移到内存
+        add bx , 2; 一次读取两个字节
+
         loop .readw
 
         popa
@@ -103,13 +109,12 @@ print:
         or al, al
         jz .print_done
 
-        mov ah, 0x0e ; 0000 黑色背景 1110 浅灰色，默认颜色
+        mov ah, 0x0e; 
         int 0x10;
         jmp .print_loop
     .print_done:
         ret
 
-    load_base dd 0x10000;
-    message db "Loader is loading...", 13, 10, 0
+    message db "Boot is loading...", 13, 10, 0
     times 510-($-$$) db 0
     db 0x55, 0xaa
--- os/src/boot/loader.asm
+++ os/src/boot/loader.asm
@@ -1,9 +1,9 @@
-base_address equ 0x10000
+%include "boot.inc"
 
-section loader vstart=base_address
+section loader vstart=LOADER_BASE_ADDR
 
-mov si, message_loaded
-call print
+    mov si, message_start_loader
+    call print
 
 finish:
     sti ; open interrupt
@@ -17,10 +17,10 @@ print:
         or al, al
         jz .print_done
 
-        mov ah, 0x0e ; 0000 黑色背景 1110 浅灰色，默认颜色
+        mov ah, 0x0e;
         int 0x10;
         jmp .print_loop
     .print_done:
         ret
 
-message_loaded db "Loader is started", 13, 10, 0
+message_start_loader db "Loader is starting...", 13, 10, 0
