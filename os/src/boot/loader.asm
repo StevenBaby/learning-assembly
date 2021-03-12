d10b0d27c0c377b5ae38fe67a1c9ab5777df8ea2
Steven Kang <kangweibaby@163.com>
2021-03-12 22:07:40 +0800

    add gdt descripter

--- os/src/boot/loader.asm
+++ os/src/boot/loader.asm
@@ -1,14 +1,43 @@
 %include "boot.inc"
 
+LOADER_STACK_TOP equ LOADER_BASE_ADDR
+
 section loader vstart=LOADER_BASE_ADDR
+    jmp start
+
+gdt_base:   GDTDescriptor 0,        0, 0
+gdt_code:   GDTDescriptor 0,        0xfffff, GDT_CODE_ATTRIBUTE
+gdt_data:   GDTDescriptor 0,        0xfffff, GDT_DATA_ATTRIBUTE
+gdt_video:  GDTDescriptor 0xb8000,  0x0ffff, GDT_VIDEO_ATTRIBUTE
+gdt_size:   equ ($ - gdt_base)
+gdt_limit:  equ (gdt_size - 1)
+gdt_ptr:    dw gdt_limit
+            dd gdt_base
+
+SELECTOR_CODE   equ (1 << 3) | TI_GDT | RPL0
+SELECTOR_DATA   equ (2 << 3) | TI_GDT | RPL0
+SELECTOR_VIDEO  equ (3 << 3) | TI_GDT | RPL0
+
+total_memory_bytes dd 0
+ards_count  dw 0
+ards_buffer times 128 - ($ - $$) db 0
+
+start:
+    ; xchg bx, bx
 
     mov si, message_start_loader
     call print
 
+    ; xchg bx, bx
     ; detect memory
     xor ebx, ebx
     mov edx, 0x534d4150
     mov di, ards_buffer
+
+    mov si, message_detect_momory_e820
+    call print
+
+    ; 内存检测可能会修改内存内容，待验证
 
 .check_memory_r820:
     mov eax, 0x0000e820
@@ -40,6 +69,9 @@ section loader vstart=LOADER_BASE_ADDR
     jmp .get_memory_ok
 
 .get_memory_e801:
+    mov si, message_detect_momory_e801
+    call print
+
     mov ax, 0xe801
     mul cx
     shl edx, 16
@@ -57,6 +89,9 @@ section loader vstart=LOADER_BASE_ADDR
     jmp .get_memory_ok
 
 .get_memory_e88:
+    mov si, message_detect_momory_e88
+    call print
+
     mov ah, 0x88
     int 0x15
     jc .get_memory_failure
@@ -72,7 +107,28 @@ section loader vstart=LOADER_BASE_ADDR
 
 .get_memory_ok:
     mov [total_memory_bytes], edx
-    xchg bx, bx
+
+    ; xchg bx, bx
+; prepare for protect mode
+    mov si, message_prepare_protect_mode
+    call print
+
+    cli
+
+    lgdt [gdt_ptr]
+
+    ; open a20 address line
+    in al, 0x92
+    or al, 0000_0010b
+    out 0x92, al
+
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    ; xchg  bx, bx
+
+    jmp dword SELECTOR_CODE:protect_mode_start
 
 .get_memory_failure:
 finish:
@@ -95,8 +151,26 @@ print:
     .print_done:
         ret
 
-message_start_loader db "Loader is starting...", 13, 10, 0
+message_start_loader            db "Loader is starting...", 13, 10, 0
+message_detect_momory_e820      db "Detecting memory e820...", 13, 10, 0
+message_detect_momory_e801      db "Detecting memory e801...", 13, 10, 0
+message_detect_momory_e88       db "Detecting memory e88...", 13, 10, 0
+message_prepare_protect_mode    db "Preparing protect mode...", 13, 10, 0
+; placeholder times 1024 - ($ - $$) db 0xff
 
-total_memory_bytes dd 0
-ards_count  dw 0
-ards_buffer times 600 db 0
+[bits 32]
+
+protect_mode_start:
+    xchg bx, bx
+    mov ax, SELECTOR_DATA
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov esp, LOADER_STACK_TOP
+    mov ax, SELECTOR_VIDEO
+
+    xchg bx, bx
+    mov gs, ax
+    xchg ebx, ebx
+
+    jmp $
